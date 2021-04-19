5fe7b9fa5a8098375cfd02eb9f400e09acc975fc
Kyle Grewe <kyle.grewe@caci.com>
2021-04-19 15:18:37 -0400

    Added header links

--- gattic-ui/src/app/app.module.ts
+++ gattic-ui/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './modules/home/home.module';
+import { SettingsModule } from './modules/settings/settings.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    SettingsModule,
+    OrdersModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ gattic-ui/src/app/modules/orders/orders-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './pages/orders/orders.component';
+
+const routes: Routes = [
+  { path: "orders", component: OrdersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule { }
