671f7c10d33dc639b61640590c8af1b63b16ac4c
Kyle Grewe <kyle.grewe@caci.com>
2021-04-20 23:04:21 -0400

    Changed project name to trade-helper

--- trade-helper-ui/e2e/src/app.e2e-spec.ts
+++ trade-helper-ui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('gattic-ui app is running!');
+    expect(await page.getTitleText()).toEqual('trade-helper app is running!');
   });
 
   afterEach(async () => {
--- trade-helper-ui/src/app/app.component.spec.ts
+++ trade-helper-ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gattic-ui'`, () => {
+  it(`should have as title 'trade-helper'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gattic-ui');
+    expect(app.title).toEqual('trade-helper');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gattic-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('trade-helper app is running!');
   });
 });
