941693ea13c287b4bbef770ac8b43dae307f929f
Kyle Grewe <kyle.grewe@caci.com>
2021-05-01 02:26:06 -0400

    Added clock endpoint

--- tradehelper-api/src/main/java/com/tradehelper/api/controllers/AlpacaController.java
+++ tradehelper-api/src/main/java/com/tradehelper/api/controllers/AlpacaController.java
@@ -17,6 +17,7 @@ import net.jacobpeterson.alpaca.enums.order.OrderStatus;
 import net.jacobpeterson.alpaca.rest.exception.AlpacaAPIRequestException;
 import net.jacobpeterson.domain.alpaca.account.Account;
 import net.jacobpeterson.domain.alpaca.asset.Asset;
+import net.jacobpeterson.domain.alpaca.clock.Clock;
 import net.jacobpeterson.domain.alpaca.order.Order;
 import net.jacobpeterson.domain.alpaca.position.Position;
 
@@ -28,23 +29,25 @@ import net.jacobpeterson.domain.alpaca.position.Position;
  */
 @RestController
 public class AlpacaController {
-	
-    /**
-     * Base url
-     * @return Welcome message
-     */
-    @RequestMapping("/")
-    public String index(){
-        return "Welcome to TradeHelper API";
-    }
-    
-    /**
-     * Gets account information
-     * @return Account
-     * @throws Exception
-     */
-    @RequestMapping("/account")
-    public Account getAccount() throws Exception {
+
+	/**
+	 * Base url
+	 * 
+	 * @return Welcome message
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "Welcome to TradeHelper API";
+	}
+
+	/**
+	 * Gets account information
+	 * 
+	 * @return Account
+	 * @throws Exception
+	 */
+	@RequestMapping("/account")
+	public Account getAccount() throws Exception {
 		try {
 			return PropertiesUtility.getAlpacaAPI().getAccount();
 		} catch (AlpacaAPIRequestException e) {
@@ -52,15 +55,16 @@ public class AlpacaController {
 		} catch (Exception e) {
 			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
 		}
-    }
-    
-    /**
-     * Gets all open positions
-     * @return List of open positions
-     * @throws Exception
-     */
-    @RequestMapping("/positions/open")
-    public List<Position> getOpenPositions() throws Exception {
+	}
+
+	/**
+	 * Gets all open positions
+	 * 
+	 * @return List of open positions
+	 * @throws Exception
+	 */
+	@RequestMapping("/positions/open")
+	public List<Position> getOpenPositions() throws Exception {
 		try {
 			return PropertiesUtility.getAlpacaAPI().getOpenPositions();
 		} catch (AlpacaAPIRequestException e) {
@@ -68,16 +72,16 @@ public class AlpacaController {
 		} catch (Exception e) {
 			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
 		}
-    }
-    
-    /**
-     * Gets all active US assets
-     * 
-     * @return List of active US assets
-     * @throws Exception
-     */
-    @RequestMapping("/assets/active")
-    public List<Asset> getUSAssets() throws Exception {
+	}
+
+	/**
+	 * Gets all active US assets
+	 * 
+	 * @return List of active US assets
+	 * @throws Exception
+	 */
+	@RequestMapping("/assets/active")
+	public List<Asset> getUSAssets() throws Exception {
 		try {
 			return PropertiesUtility.getAlpacaAPI().getAssets(AssetStatus.ACTIVE, "us_equity");
 		} catch (AlpacaAPIRequestException e) {
@@ -85,74 +89,79 @@ public class AlpacaController {
 		} catch (Exception e) {
 			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
 		}
-    }
-    
-    /**
-     * Get all orders
-     * @return List of alpaca orders
-     */
-    @RequestMapping("/orders/all")
-    public List<Order> getOrders() throws Exception {
-    	
+	}
+
+	/**
+	 * Gets the current market clock
+	 * 
+	 * @return Current market clock
+	 * @throws Exception
+	 */
+	@RequestMapping("/clock")
+	public Clock getClock() throws Exception {
 		try {
-			return PropertiesUtility.getAlpacaAPI().getOrders(
-		            OrderStatus.ALL,
-		            null,
-		            ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")),
-		            null,
-		            null,
-		            true,
-		            Arrays.asList("AAPL", "TSLA"));
+			return PropertiesUtility.getAlpacaAPI().getClock();
 		} catch (AlpacaAPIRequestException e) {
 			throw new AlpacaException("Can't reach alpaca.  Check credentials in alpaca.properties", e);
 		} catch (Exception e) {
 			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
 		}
-    }
-    
-    /**
-     * Get open orders
-     * @return List of alpaca orders
-     */
-    @RequestMapping("/orders/open")
-    public List<Order> getOpenOrders() throws Exception {
-    	
+	}
+
+	/**
+	 * Get all orders
+	 * 
+	 * @return List of alpaca orders
+	 */
+	@RequestMapping("/orders/all")
+	public List<Order> getOrders() throws Exception {
+
 		try {
-			return PropertiesUtility.getAlpacaAPI().getOrders(
-		            OrderStatus.OPEN,
-		            null,
-		            ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")),
-		            null,
-		            null,
-		            true,
-		            Arrays.asList("AAPL", "TSLA"));
-		} catch (AlpacaAPIRequestException e) {
-			throw new AlpacaException("Can't reach alpaca.  Check credentials in alpaca.properties", e);
-		}catch (Exception e) {
-			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
-		}
-    }
-    
-    /**
-     * Get closed orders
-     * @return List of alpaca orders
-     */
-    @RequestMapping("/orders/closed")
-    public List<Order> getClosedOrders() throws Exception {
-    	
-		try {
-			return PropertiesUtility.getAlpacaAPI().getOrders(
-		            OrderStatus.CLOSED,
-		            null,
-		            ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")),
-		            null,
-		            null,
-		            true,
-		            Arrays.asList("AAPL", "TSLA"));
+			return PropertiesUtility.getAlpacaAPI().getOrders(OrderStatus.ALL, null,
+					ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")), null, null, true,
+					Arrays.asList("AAPL", "TSLA"));
 		} catch (AlpacaAPIRequestException e) {
 			throw new AlpacaException("Can't reach alpaca.  Check credentials in alpaca.properties", e);
 		} catch (Exception e) {
 			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
 		}
-    }
+	}
+
+	/**
+	 * Get open orders
+	 * 
+	 * @return List of alpaca orders
+	 */
+	@RequestMapping("/orders/open")
+	public List<Order> getOpenOrders() throws Exception {
+
+		try {
+			return PropertiesUtility.getAlpacaAPI().getOrders(OrderStatus.OPEN, null,
+					ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")), null, null, true,
+					Arrays.asList("AAPL", "TSLA"));
+		} catch (AlpacaAPIRequestException e) {
+			throw new AlpacaException("Can't reach alpaca.  Check credentials in alpaca.properties", e);
+		} catch (Exception e) {
+			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
+		}
+	}
+
+	/**
+	 * Get closed orders
+	 * 
+	 * @return List of alpaca orders
+	 */
+	@RequestMapping("/orders/closed")
+	public List<Order> getClosedOrders() throws Exception {
+
+		try {
+			return PropertiesUtility.getAlpacaAPI().getOrders(OrderStatus.CLOSED, null,
+					ZonedDateTime.of(2020, 12, 23, 0, 0, 0, 0, ZoneId.of("America/New_York")), null, null, true,
+					Arrays.asList("AAPL", "TSLA"));
+		} catch (AlpacaAPIRequestException e) {
+			throw new AlpacaException("Can't reach alpaca.  Check credentials in alpaca.properties", e);
+		} catch (Exception e) {
+			throw new MissingPropertiesFileException("Can't find alpaca.properties", e);
+		}
+	}
 }
